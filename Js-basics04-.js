c4eaa1d8ce1b09f3903ffae1b28e3bcfa9c88a18
vbaskaran3 <vbaskaran3@gmail.com>
2022-12-17 18:00:16 -0800

    JS Basics

--- Js-basics04-.js
+++ Js-basics04-.js
@@ -15,7 +15,7 @@
 
 //3 write a function that returns boolean flag if a number is between 5000 and 9999
 
-function isBoolean(num) {
+function isBoolean(num) 
   {
   if (num>5000 && num<9999) 
       return true;
